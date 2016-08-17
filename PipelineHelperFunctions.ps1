efcd42f29536d2b7e3bb9f85f8ebc11ff04cc667
Rod Stewart <rodstewart@gmail.com>
2016-08-17 18:41:44 +0000

    Adds PipelineHelperFunctions - useful for finding IO types

---
+++ PipelineHelperFunctions.ps1
@@ -0,0 +1,20 @@
+﻿Function Get-InputType { 
+    [cmdletbinding()]
+    param([string]$command)
+    try { Get-Help $command | select-object -ExpandProperty inputtypes }
+    catch { Write-Error -Exception $_.exception } 
+}
+
+Function Search-ReturnValue { 
+    [cmdletbinding()]
+    param([string]$ReturnValue)
+    try { Get-Help * | Where-Object { $_.returnvalues.returnvalue.type.name -eq $ReturnValue } }
+    catch [System.NullReferenceException] { } 
+    catch { Write-Error -Exception $_.exception } 
+}
+
+Get-InputType Stop-service
+Search-ReturnValue -ReturnValue "System.ServiceProcess.ServiceController"
+
+Get-InputType Remove-ADGroup
+Search-ReturnValue -ReturnValue "Microsoft.ActiveDirectory.Management.ADGroup"
