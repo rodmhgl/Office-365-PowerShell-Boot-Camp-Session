39c5984870e9955b3a662a646410adfb648da0c0
Rod Stewart <rodstewart@gmail.com>
2016-08-17 01:00:44 +0000

    Adds AD Forest Creation Demo
    Creates a new AD Domain Controller in a new AD Forest

---
+++ Create-ADForest.ps1
@@ -0,0 +1,13 @@
+﻿# Import required module 
+Import-Module ServerManager
+# Verify AD-Domain-Services is not currently installed
+Get-WindowsFeature | Where-Object Installed
+
+# Install the required features to create a new forest, along with their management tools
+Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools
+
+# Install a new active directory forest configuration
+Install-ADDSForest -DomainName 'poshdev.local' -SkipPreChecks -InstallDns -NoRebootOnCompletion
+
+# Restart computer to complete installation
+Restart-Computer
