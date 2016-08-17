5c82ca5fbc1c17e1a3f0495931d44f052bddf62d
Rod Stewart <rodstewart@gmail.com>
2016-08-17 18:32:33 +0000

    Adds PowerShell example for removing disabled users

---
+++ Remove-DisabledUsers.ps1
@@ -0,0 +1,5 @@
+﻿# Retrieve all users from AD, Filtered with Where-Object, Delete disabled users
+Get-ADUser -Filter * | where-object {$_.enabled -eq $false}  | Remove-ADUser -WhatIf
+
+# Or use the filter parameter of Get-ADUser to filter objects instead of where-object
+Get-ADUser -Filter {Enabled -eq $false} | Remove-ADUser -WhatIf
